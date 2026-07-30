3ac5f04c35e7d40473979e452146fc4a1d4bbf7d
Nikhil Kumar <kumarn05668@gmail.com>
2026-07-31 00:48:24 +0530

    Update r

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -2,4 +2,4 @@
 // API Configuration
 // ===============================
 
-export const BASE_URL = "https://sahu-enterprises-ecommerce.onrender.com/api";
+const BASE_URL = "https://sahu-enterprises-ecommerce.onrender.com/api";
