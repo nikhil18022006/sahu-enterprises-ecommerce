8ddbc2d985e9895864b86f48326f5a101c70c551
Nikhil Kumar <kumarn05668@gmail.com>
2026-07-31 16:52:03 +0530

    admin updae

--- frontend/js/navbar.js
+++ frontend/js/navbar.js
@@ -1,11 +1,11 @@
 const hamburger = document.getElementById("hamburger");
-const menu = document.getElementById("menu");
+const menu = document.querySelector(".menu");
 
 if (hamburger && menu) {
 
-    hamburger.addEventListener("click", function () {
+    hamburger.addEventListener("click", () => {
 
-        menu.classList.toggle("hide-menu");
+        menu.classList.toggle("active");
 
     });
 
