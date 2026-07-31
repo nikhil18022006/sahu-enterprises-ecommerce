c93ca330654fa837ae0ce3d8fe29c47a37096432
Nikhil Kumar <kumarn05668@gmail.com>
2026-07-31 17:17:29 +0530

    admin updae

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,5 +1,2 @@
-// ===============================
-// API Configuration
-// ===============================
-
 const BASE_URL = "https://sahu-enterprises-ecommerce.onrender.com/api";
+window.BASE_URL = BASE_URL;
